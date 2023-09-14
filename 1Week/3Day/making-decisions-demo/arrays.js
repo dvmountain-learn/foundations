76271060574c30f03d2bc36f1fdc02f1f891bbc6
SENGHORT KHEANG <senghort.rupp@gmail.com>
2023-09-14 12:40:01 -0500

    Implementation of pokemon in array of 3 day.

--- 1Week/3Day/making-decisions-demo/arrays.js
+++ 1Week/3Day/making-decisions-demo/arrays.js
@@ -3,3 +3,62 @@
     with Jon being allowed to flee his fight with Jamie.
 */
 
+let backpack = ['poke ball', 'poke ball', 'potion', 'beef jerky', 'running shoes']
+let pokemon = []
+
+backpack.push('oreos')
+backpack.push('oreos')
+console.log(backpack)
+
+
+backpack.push('pokedex')
+pokemon.push('pikachu')
+console.log(backpack)
+console.log(pokemon)
+
+for (let i = 0; i <= 4; i++) {
+    pokemon.push('pedgey')
+}
+
+backpack.splice(3, 4)
+console.log('')
+console.log(backpack)
+
+let newPokemon = 'Charizard'
+pokemon.push(newPokemon)
+
+console.log('after catching charizard, our pokemon is now:')
+console.log(pokemon)
+
+// someone asks to see your cutest pokemon
+// which is pikachu, duh
+// so you show them pikachu
+console.log('Here is my pikachu')
+let cutestPokemon = pokemon[0]
+console.log(cutestPokemon)
+
+// a geneticist crosses your path, and asks if he can clone your
+// pidgeys, for science
+// you agree, if he'll give you the lead status of a jynx he is currently holding
+let clonedPidgeys = pokemon.splice(1, 5)
+console.log('Here are your cloned pidgeys')
+console.log(clonedPidgeys)
+
+// let's make sure your pokemon stayed the same (no pidgeys were lost)
+console.log('checking to make sure my pokemon are all still here:')
+console.log(pokemon)
+
+backpack.unshift('jynx status')
+console.log('backpack after adding the status:')
+console.log(backpack)
+
+
+
+
+
+
+
+
+
+
+
---
+++ 1Week/4Day/practice.js
@@ -0,0 +1,47 @@
+
+let namesArr = ['Andrew', 'Boob', 'Suaden', 'Altan']
+console.log(namesArr)
+
+// let popped = namesArr.pop()
+// console.log(popped)
+
+console.log(namesArr)
+namesArr.push('Jorly')
+console.log(namesArr)
+
+// namesArr.splice()
+// console.log(namesArr)
+// namesArr.splice(2, 4)
+// console.log(namesArr)
+
+// for (let i = 0; i < namesArr.length; i++) {
+//     console.log(namesArr[i])
+// }
+
+namesArr.forEach((name) => {
+    console.log(name)
+})
+
+let numsArr = [1, 3, 10, -20, 5, 7, 50]
+
+let max = null
+let min = null
+// This for loop
+// for (let i = 0; i < numsArr.length; i++) {
+//     let elements = numsArr[i]
+//     if (min === null || min > elements) { 
+//         min = elements
+//     }
+//     if (max === null || max < elements) {
+//         max = elements
+//     }
+// }
+
+// This is foreach loop
+numsArr.forEach((num) => {
+    let ele = num
+    if (min === null || min > ele) min = ele
+    if (max === null || max < ele) max = ele
+})
+
+console.log(min, max)
