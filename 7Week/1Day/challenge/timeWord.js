2ba69e324c5084aa925ac5af0c3941e4626e153b
SENGHORT KHEANG <senghort.rupp@gmail.com>
2023-10-25 10:06:56 -0500

    95% finish code in lab.

---
+++ 7Week/1Day/challenge/timeWord.js
@@ -0,0 +1,3 @@
+function timeWord(stringTime) {
+    
+}
---
+++ 7Week/2Day/challenge/scale.js
@@ -0,0 +1,13 @@
+
+// let nums = []
+// for (let i = 0; i < 100; i++) {
+//   nums.push(i)
+// }
+// nums = nums.reverse()
+
+let nums = []
+for (let i = 0; i < 100; i++) {
+  nums.unshift(i)
+}
+
+console.log(nums)
